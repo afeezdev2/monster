6415079801f149103e6deb5bca3b34527fed3607
afeezdev <afeezdev@gmail.com>
2020-01-11 10:43:11 +0100

    Updates

---
+++ precache-manifest.9f9471178eb5f6c320114ce9ef0d16aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa393522677f1fdc832fbfce99fd6695",
+    "url": "/monster1/index.html"
+  },
+  {
+    "revision": "beb5831b55f419a43bb3",
+    "url": "/monster1/static/css/main.2dd338fc.chunk.css"
+  },
+  {
+    "revision": "1093a6240466003a5984",
+    "url": "/monster1/static/js/2.3a82cee6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster1/static/js/2.3a82cee6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "beb5831b55f419a43bb3",
+    "url": "/monster1/static/js/main.def312a5.chunk.js"
+  },
+  {
+    "revision": "97e0e2d78731aca3983a",
+    "url": "/monster1/static/js/runtime-main.e8ad0358.js"
+  }
+]);
